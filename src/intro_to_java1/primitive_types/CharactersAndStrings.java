661a24954e2744ccf91903800d87a9e50bbcdb2f
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-11-03 21:13:31 +0200

    Added Primitive Types - Olimpia

--- src/intro_to_java1/primitive_types/CharactersAndStrings.java
+++ src/intro_to_java1/primitive_types/CharactersAndStrings.java
@@ -4,12 +4,14 @@ public class CharactersAndStrings {
     //char - 16 bits - 2 bytes
 
     public static void main(String[] args) {
+        char firstChar ='4';
+        System.out.println(firstChar);
         System.out.println("*****Char Maximum Numeric Values*****");
         System.out.println("Char MAX: " + (Character.MAX_VALUE + 0));
         System.out.println("Char MIN: " + (Character.MIN_VALUE + 0));
 
         System.out.println("*****Char Maximum Character Values*****");
-        System.out.println("Char MAX: " + (Character.MAX_VALUE + 0));
-        System.out.println("Char MIN: " + (Character.MIN_VALUE + 0));
+        System.out.println("Char MAX: " + Character.MAX_VALUE );
+        System.out.println("Char MIN: " + Character.MIN_VALUE );
     }
 }
