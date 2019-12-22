3639f2879ed50c1de9825952a6e379b410093ab5
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-12-22 11:22:15 +0200

    Algorithms and Data Structures - initial commit

---
+++ src/algorithmsAndDataStructure/algorithms/Fibonacci.java
@@ -0,0 +1,22 @@
+package algorithmsAndDataStructure.algorithms;
+/*
+Display the Fibonacci sequence with n numbers
+ */
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0, b = 1, aux, n, count = 1;
+        Scanner myNumber = new Scanner(System.in);
+        n = myNumber.nextInt();
+
+        while (count <= n) {
+            System.out.println(a + " ");
+            count++;
+            aux = b;
+            b = a + b;
+            a = aux;
+        }
+    }
+}
